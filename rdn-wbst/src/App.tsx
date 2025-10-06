e28f17dfcb4a530abbf150ebe13b1c4e6661dc61
45tera <45terax@gmail.com>
2025-10-07 00:50:16 +0800

    new:trial-website

---
+++ rdn-wbst/src/App.tsx
@@ -0,0 +1,16 @@
+
+function App() {
+
+  return (
+    <>
+      <body>
+        <div className="flex flex-col justify-center items-center min-h-screen text-center">
+          <h1 className="text-2xl font-bold mb-4">When is your birthday?</h1>
+          <p>My birthday is on [insert date here]</p>
+        </div>
+      </body>
+    </>
+  )
+}
+
+export default App
